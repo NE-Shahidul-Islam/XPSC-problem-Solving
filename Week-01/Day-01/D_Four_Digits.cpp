4db7c534576fab8759cfb19afdcef1c0dac8752c
Shahidul Islam <168478159+NE-Shahidul-Islam@users.noreply.github.com>
2024-10-26 23:40:50 +0600

    Add files via upload
    Added week-1 Day-01

---
+++ Week-01/Day-01/D_Four_Digits.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+#define FIO                      \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr)
+#define ll long long
+#define in(x) cin>>x
+#define out(x) cout<<x
+#define vin(v) for(int i=1; i<=n; i++) in(v[i])
+#define vout(v) for(int i=1; i<=n; i++) out(v[i])
+#define llloop(i, a, b)  for (ll i = a; i <= b; i++)
+#define rllloop(i, b, a)  for (ll i = b - 1; i >= a; i--)
+#define iloop(i, a, b)  for (int i = a; i <= b; i++)
+#define riloop(i, b, a)  for (int i = b - 1; i >= a; i--)
+#define sob(x)  x.begin(), x.end()
+#define rsob(x)  x.rbegin(), x.rend()
+#define sz(x)  (ll) x.size()
+#define pb  push_back
+#define ppb  pop_back
+#define M 1e9 + 7
+#define no cout<<"NO"<<endl
+#define yes cout<<"YES"<<endl
+#define nl '\n'
+#define bits(x) __builtin_popcountll(x)
+#define vl vector<ll>
+#define vi vector<int>
+#define pll pair<ll, ll>
+#define vpll vector<pll>
+#define uni(x) x.erase(unique(all(x)), x.end())
+
+const int N=1e6+9;
+int a[N],b[N];
+using namespace std;
+void naruto()
+{
+   string s; cin >> s;
+   int len = s.size();
+   string a;
+   if(len == 4) cout << s << nl; 
+   else{
+    for(int i = len ; i<4; i++){
+        a.push_back('0');
+    }
+
+    cout << a+s << nl;
+   }
+
+
+  
+   
+}
+
+
+signed main()
+{
+    FIO;
+   naruto();
+    return 0;
+}
