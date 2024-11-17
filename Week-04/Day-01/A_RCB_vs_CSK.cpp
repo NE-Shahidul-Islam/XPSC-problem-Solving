0087baf0876023b4a48e29a0ecc25f37a72c7614
Shahidul Islam <168478159+NE-Shahidul-Islam@users.noreply.github.com>
2024-11-17 23:18:32 +0600

    Add files via upload
    Week-04 day-01 codes added

---
+++ Week-04/Day-01/A_RCB_vs_CSK.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+#define FIO                      \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr)
+#define ll long long
+#define in(x) cin>>x
+#define out(x) cout<<x
+#define vin(v) for(int i=1; i<=n; i++) in(v[i])
+#define vout(v) for(int i=1; i<=n; i++) out(v[i])
+#define llloop(i, a, b)  for (ll i = a; i <= b; i++)
+#define rllloop(i, b, a)  for (ll i = b - 1; i >= a; i--)
+#define iloop(i, a, b)  for (int i = a; i <= b; i++)
+#define riloop(i, b, a)  for (int i = b - 1; i >= a; i--)
+#define sob(x)  x.begin(), x.end()
+#define rsob(x)  x.rbegin(), x.rend()
+#define sz(x)  (ll) x.size()
+#define pb  push_back
+#define ppb  pop_back
+#define M 1e9 + 7
+#define yes cout<<"RCB"<<endl
+#define no cout<<"CSK"<<endl
+#define nl '\n'
+#define bits(x) __builtin_popcountll(x)
+#define vl vector<ll>
+#define vi vector<int>
+#define pll pair<ll, ll>
+#define vpll vector<pll>
+#define uni(x) x.erase(unique(all(x)), x.end())
+
+const int N=1e6+9;
+int a[N],b[N];
+using namespace std;
+void naruto()
+{
+   int a, b;
+   cin >> a >> b;
+   b+=18;
+   if(a>=b) yes;
+   else no;
+
+
+  
+   
+}
+
+
+signed main()
+{
+    FIO;
+    naruto();
+    return 0;
+}
