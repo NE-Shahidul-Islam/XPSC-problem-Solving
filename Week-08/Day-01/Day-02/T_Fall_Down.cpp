f6d09bbf90d599dcea568a7f6af186976836a3b7
Shahidul Islam <168478159+NE-Shahidul-Islam@users.noreply.github.com>
2024-12-18 20:52:13 +0600

    Add files via upload
    Day-02 week-08 codes added

---
+++ Week-08/Day-01/Day-02/T_Fall_Down.cpp
@@ -0,0 +1,178 @@
+/*
+                        NARUTO UZUMAKI...
+                                DATTEBYO!!...
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⣿⣿⣿⣿⣵⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⣡⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠡⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣮⣍⡛⠿⢿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣾⣿⣿⣿⣿⣿⣿⣶⣬⣙⠻⢿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢋⣴⣿⣿⣿⣿⣿⡿⣿⣿⣿⣏⢿⣿⣿⣿⠿⠿⠿⠿⠿⠿⠿⣿⣿⣿⣿⣟⢿⣿⣿⣿⣿⣿⣿⣿⣾⣿⣿⣿⣿⣷⣿⣿⡿⠟⣋⣤⣾⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⠿⣫⣶⣿⣿⣿⣿⣿⣿⣿⣿⡌⠹⢿⣿⣦⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠛⠄⢿⣿⣿⣿⣿⣿⠙⢿⣿⣿⣿⣿⠟⢊⢭⣶⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⠟⣡⣾⣿⣿⣿⡿⣿⣿⣿⣿⣿⣿⠁⠀⠀⠙⠿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣿⣿⣽⣿⠀⣿⣿⣿⣿⡿⢦⡙⢾⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣅⣬⣵⣶⣶⠍⣿⣷⢸⣿⣿⣿⣿⣿⠀⠀⢶⣶⣶⣮⣙⠌⢻⣿⣿⣶⣶⣶⣶⣤⣄⣀⣀⠀⠀⠀⠀⢻⣿⣿⣾⠀⣿⣿⣿⣿⣿⡄⣍⣀⠹⢿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣰⣿⠋⠀⢻⣿⣿⣿⡿⢰⣤⣼⣿⣿⣿⣿⣿⣦⣿⠟⢛⠛⠿⠟⣙⣿⣿⣿⣿⣿⠲⣄⠀⠻⣿⣿⡄⣿⢨⣿⣿⣿⣿⡸⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⢇⣿⠃⠀⠀⠈⢿⣿⣿⡇⠸⣿⣿⣿⣿⣟⣻⣿⠏⢠⡶⠛⠛⢶⣾⣿⣿⣿⣿⣛⣿⣦⣾⡆⠀⠹⣿⡆⠘⢻⣿⣿⣿⣿⣇⢽⣿⣿⣾⣟⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⡏⢸⠋⠀⠀⠀⠀⠘⢿⣿⡧⢠⣾⣿⣿⣿⣿⡿⢁⡄⢻⣇⠛⠳⠀⣿⣿⣿⣿⢻⣿⣿⠛⣿⠀⠀⠀⠑⡂⠀⠀⠙⢿⣿⡿⣶⡄⣿⣿⣿⣿⣟⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⠁⢣⡆⠀⠀⠀⠀⠀⠈⢿⣯⠘⢿⣿⡿⠋⣿⣷⣦⣍⣀⣉⣋⣁⣴⣿⣿⣯⣅⣹⣿⣿⣷⣿⠀⠀⠀⠀⠁⠀⠀⠀⠠⡙⢿⣿⢷⠸⣾⣿⣿⣿⣻⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣠⣾⡇⠀⠀⠀⠀⠀⠀⠈⢻⠠⠾⠿⠿⠟⠛⠛⠛⠛⠛⠻⠿⠿⠿⠿⠿⠿⣿⣿⣿⠏⢩⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⣷⡆⡙⢿⡇⠻⣿⣿⡿⣽⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠙⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⡙⠇⣿⢦⡉⠘⣿⣷⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡃⠀⠀⢀⡄⢰⣶⣶⣷⣦⣍⡓⠨⢙⠻⣿⣿⣿⠟⣻⣿⣿⠆⣶⣶⡶⠠⠀⣠⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⠹⠿⠝⠂⠙⠛⠛⠋⠉⠉
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁⢤⢻⣿⠇⣿⣿⠋⣡⡄⢂⡀⠁⠲⡁⠀⠙⣡⣾⣿⣿⣿⡀⠛⠉⢁⡤⠁⠀⠈⣉⠻⢿⣷⣶⡆⣦⣤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠸⠈⠀⣿⠀⣿⣿⡀⢻⡀⢋⡹⢈⣆⠱⡄⣺⣿⣿⣿⣿⡿⠋⠀⣰⠋⣰⠁⡏⣱⠌⡿⠀⣽⣿⡇⣿⣿⠁⠆⢠⡆⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⢰⡎⠀⢸⢠⣿⣿⣷⣌⡛⠶⠶⠛⢛⣠⣿⣿⣿⣿⣿⣿⠃⢀⣾⣿⣄⠻⠶⠬⠥⠜⢁⣼⣿⣿⡇⣿⡇⠀⣄⣼⢠⡀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠸⠀⢀⡄⢸⣿⣿⣿⣿⠿⠿⢛⣿⣿⣿⣿⣿⣿⣿⣿⡿⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⡏⡀⠀⢿⠏⣼⣷⡄⠀⠈⢿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⡀⠈⢷⣼⠟⣛⣭⣴⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣾⣿⣿⣿⣿⡟⢻⣏⡛⠿⣿⣿⣿⣿⡇⠀⠀⡠⠊⣰⣿⣿⣇⠀⠀⠈⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⠙⣶⣦⠸⣿⣿⣿⣿⡿⠛⣹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡍⡛⢿⣿⣳⣦⣍⡛⢿⢃⣴⣾⠆⣴⣿⣿⣿⣿⡄⠀⠀⠻⢿⣿⠿⠿⠿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⡙⣿⠀⣿⡿⢛⣡⣶⣿⣿⣿⣿⣿⣿⡿⠛⠿⡿⠿⠿⢿⣿⣿⣿⣷⣿⣷⣌⡹⢿⣿⣿⡦⠘⠿⢡⢸⣿⣿⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⢀⠀
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠷⠄⠆⠙⠶⠛⠛⠛⠁⠻⣿⣿⣿⣿⣷⣄⠂⠊⠠⢀⣼⣿⣿⣿⣿⣿⡘⢿⣿⣦⡙⠟⣠⡿⢀⠀⣾⣿⣿⣿⣿⣿⣿⣿⣦⡀⠀⠀⠀⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⢸⣿⣿⣿⣿⣿⣷⣤⣴⢿⡿⠿⣿⡛⢿⣿⣿⣦⡹⣿⡟⢠⣿⠇⢾⡇⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣄⠀⠀⠙⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣍⣉⠉⠶⠶⠶⠶⠶⡶⠶⠒⣒⣠⣤⣿⣿⣿⣷⡌⢠⠉⣴⡇⠀⠀⠀⠀⠉⠉⠛⣿⣿⣿⣿⣿⣿⣷⣄⡀⢻
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⠏⢰⠆⠀⠘⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠁⢀⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⡡⠀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢂⣾⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠘⢶⢌⡙⠿⠿⣿⣿⣿⣿⠿⠟⠋⠁⡠⠊⠀⠀⠅⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣼⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣵⣿⣿⣷⣶⡠⢀⠄⠀⡀⠀⠂⢈⠀⠀⡡⢊⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢱⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⡁⣽⣿⡿⡋⡊⢀⠅⡠⠈⢈⠄⠁⠁⡠⠊⣴⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢡⣾⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⡿⠿⠟⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡻⠉⠀⠈⢵⡧⡃⢊⠔⠟⡁⠢⢈⢄⣼⣿⣧⠐⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⠈⠛⢿⣿⣿⣿⣿⣿⣿⣿
+⠿⠛⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣫⡆⣼⣷⣌⠡⢀⡄⣡⠊⠔⠔⢁⣾⣿⣿⣿⠇⢀⡦⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠂⠀⠀⠀⠈⠛⠿⣿⣿⣿⣿
+*/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/detail/standard_policies.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+#define FIO                     ios::sync_with_stdio(false); cin.tie(nullptr)
+#define int                      long long
+#define ull                     unsigned long long
+#define lll                     __int128
+#define lld                     long double
+#define in(x)                   int x; cin >> x
+#define in(x,y)                 int x,y; cin >> x >> y
+#define in(x, y, z)             int x, y, z; cin >> x >> y >> z
+#define forn(i, a, b)           for(int i = a; i < b; i++)
+#define rforn(i, b, a)          for(int i = b-1; i >= a; i--)
+#define Forn(i, a, b)           for(int i = a; i <= b; i++)
+#define rForn(i, b, a)          for(int i = b; i >= a; i--)
+#define ini(x, y)               memset(x, y, sizeof(x))
+#define all(x)                  x.begin(), x.end()
+#define rall(x)                 x.rbegin(), x.rend()
+#define sz(x)                   (int)x.size()
+#define pb                      push_back
+#define ppb                     pop_back
+#define mp                      make_pair
+#define ff                      first
+#define ss                      second
+#define mod                      1000000007
+#define nl                      '\n'
+#define bits(x)                 __builtin_popcountll(x)
+#define zrbits(x)               __builtin_ctzll(x)
+#define msb(x)                     __lg(x)
+#define vi                      vector<int>
+#define vin(v,n)                for (int i = 0; i < n; i++) cin >> v[i];
+#define pii                     pair<int,int>
+#define vpii                    vector<pii>
+#define uni(x)                  x.erase(unique(all(x)), x.end())
+#define ordered_set             tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+#define multi_ordered_set       tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update>
+#define mxheap                  priority_queue<int>
+#define mnheap                  priority_queue<int, vector<int>, greater<int>>
+#define mxheap2                 priority_queue<pair<int,int>>
+#define mnheap2                 priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>>
+#define print(arr)              for(auto x: arr)cout<<x<<" ";nl;
+#define mprint(mp)              for(auto a : mp)cout<<a.first<<" "<<a.second<<endl;
+#define deb1(x)                 cout <<"(x)=["<<x << "]" <<nl
+#define deb2(x, y)              cout <<"(x,y)= ["<<x <<" "<<y <<"]" << nl
+#define deb3(x, y, z)           cout <<"(x,y,z)= ["<<x <<" "<<y << " "<<z <<"]" << nl
+
+using namespace std;
+using namespace __gnu_pbds;
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+#define rng(x,y) uniform_int_distribution<int>(x,y)(rng)
+
+const int N = 2e5 + 5;
+const int L = 20;
+const int MX = 1e9 + 10;
+const int INF = 1e18;
+
+const int dx[] = {0, -1, 0, 1, -1, -1, 1, 1};
+const int dy[] = {1, 0, -1, 0, 1, -1, -1, 1};
+
+
+//----------------Modular Arithmetic-----------------------------------
+int plusMod(int a, int b){return ((a % mod) + (b % mod)) % mod;}
+int multiMod(int a, int b){return ((a % mod) * (b % mod)) % mod;}
+int minusMod(int a, int b){return ((a % mod) - (b % mod) + mod) % mod;}
+int binaryexp(int x, int y, int p){int res = 1;x = x % p;if (x == 0)return 0;while (y > 0){if (y & 1)res = (res * x) % p;y = y >> 1;x = (x * x) % p;}return res;} 
+int inv(int a){return binaryexp(a, mod - 2, mod);}
+int divideMod(int a, int b){return ((a % mod) * inv(b) % mod) % mod;}
+int power(int x, int y){int res = 1;x = x;if (x == 0)return 0;while (y > 0){if (y & 1)res = (res * x);y = y >> 1;x = (x * x);}return res;}
+//----------------Logarithm--------------------------------------------
+int ulog(int val, int base) {int cnt = 0; val /= base; while(val) {cnt++; val /= base;} return cnt;}
+//---------------------For combinatorics---------------------------------
+// vi fact(2e5 + 10, 1);
+ //int nCr(int n, int r){int lob = fact[n];int hor = (fact[r] % mod * fact[n - r] % mod) % mod;int ans = (lob % mod * inv(hor) % mod) % mod;return ans;}
+//------------------- Precalculation Factorial----------------------------------------
+    // for (int i = 1; i <= 2e5; i++)  fact[i] = (fact[i - 1] % mod * i % mod) % mod;
+//-------------Bits Conversion--------------------------------------------
+string decToBinary(int n){string s="";int i = 0;while (n > 0) {s =to_string(n % 2)+s;n = n / 2;i++;}return s;}
+int binaryToDecimal(string n){string num = n;int dec_value = 0;int base = 1;int len = num.length();for(int i = len - 1; i >= 0; i--){if (num[i] == '1')dec_value += base;base = base * 2;}return dec_value;}
+//------------------------Check----------------
+bool isPrime(int n){if(n<=1)return false;if(n<=3)return true;if(n%2==0||n%3==0)return false;for(int i=5;i*i<=n;i=i+6)if(n%i==0||n%(i+2)==0)return false;return true;}
+bool isPowerOfTwo(int n){if(n==0)return false;return (ceil(log2(n)) == floor(log2(n)));}
+bool isPerfectSquare(int x){if (x >= 0) {int sr = sqrt(x);return (sr * sr == x);}return false;}
+ 
+// ----------------For Sieve------------------------------------------------
+//vector<bool> is_Prime(2e5 + 10, true);
+//vector<int> primes;
+//vector<int> primeFactors(int n){vl primes;while (n % 2 == 0){primes.pb(2);n = n / 2;}for (int i = 3; i <= sqrt(n); i = i + 2) {while (n % i == 0){primes.pb(i);n = n / i;}}if (n > 2)primes.pb(n);return primes;}               
+
+void setting (){
+    
+}
+
+void Dattebyo() {
+        int n, m;
+        cin >> n >> m;
+        char a[n+1][m+1];
+
+        Forn(i,1,n){
+            Forn(j, 1, m){
+                cin >> a[i][j];
+            }
+        }
+
+    for (int col = 1; col <=m; col++){
+        int last = n;
+        for (int row = n; row >=1; row--){
+            if(a[row][col]=='o'){
+                last = row;
+                last--;
+            }
+            if(a[row][col]=='*'){
+                swap(a[row][col],a[last][col]);
+                last--;
+            }
+        }
+    }    
+
+      Forn(i,1,n){
+            Forn(j, 1, m){
+               cout <<a[i][j];
+            }
+            cout << nl;
+        }   
+      cout << nl;    
+}
+
+signed main () {
+       
+        FIO;
+        int testcase = 1;
+        cin >> testcase;
+        forn(i, 0, testcase){
+         //cout << "Case " << tc << ": ";
+                Dattebyo();
+        }
+        
+         return 0;
+}
